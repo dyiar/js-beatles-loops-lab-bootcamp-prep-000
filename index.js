47e9b032a16228e36e7cdd87918f837d77cc8162
Austin Blake <ummwhere@hotmail.com>
2017-12-01 03:59:32 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function theBeatlesPlay(musicians, instruments) {
+  const array = [];
+  for (let i=0, l = musicians.length; i<l; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array;
+}
+
+function johnLennonFacts(facts) {
+  const loudFacts = [];
+  let i=0
+  while(i< facts.length) {
+    console.log(`${facts[i]}!!!`)
+  }
+  return loudFacts
+}
