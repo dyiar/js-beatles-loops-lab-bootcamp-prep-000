17513f5ad89062430c911413c0bb7fe887192df1
Austin Blake <ummwhere@hotmail.com>
2017-12-01 04:09:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   const array = [];
-  for (let i=0, l = musicians.length; i<l; i++) {
+
+  for(let i =0, l = musicians.length; i < l; i++) {
     array.push(`${musicians[i]} plays ${instruments[i]}`)
   }
   return array;
@@ -8,20 +9,10 @@ function theBeatlesPlay(musicians, instruments) {
 
 function johnLennonFacts(facts) {
   const loudFacts = [];
-  let i=0
-  while(i< facts.length) {
+  let i = 0;
+  while(i<facts.length){
     loudFacts.push(`${facts[i]}!!!`)
     i++
   }
-  return loudFacts
-}
-
-function iLoveTheBeatles(n) {
-  const array = [];
-
-  do {
-    array.push('I love the Beatles!')
-    n++
-  } while (n < 15)
-  return array;
+  return loudFacts;
 }
